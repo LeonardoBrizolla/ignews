44eb308941e9da2420c817f4b42a1aa56ced37c7
Leonardo Brizolla <leonardobrizollars@gmail.com>
2022-05-18 17:42:00 -0300

    Página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,35 @@
 import Head from "next/head";
+import Image from "next/image";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Start | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+
+          <p>
+            Get access to all publications <br /> <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <Image
+          src="/images/avatar.svg"
+          alt="Girl coding"
+          width={400}
+          height={400}
+        />
+      </main>
     </>
   );
 }
