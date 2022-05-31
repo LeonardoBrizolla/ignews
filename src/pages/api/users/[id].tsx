72a059d402762dcc55cd522f1385206d969d1994
Leonardo Brizolla <leonardobrizollars@gmail.com>
2022-05-31 18:09:45 -0300

    Parametrização nas rotas

---
+++ src/pages/api/users/[id].tsx
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+/* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
+export default (request: NextApiRequest, response: NextApiResponse) => {
+  console.log(request.query)
+
+  const users = [
+    { id: 1, name: 'Leonardo Brizolla' },
+    { id: 2, name: 'Vinicius Brizolla' },
+    { id: 3, name: 'Leonel Brizolla' }
+  ]
+
+  return response.json(users)
+}
